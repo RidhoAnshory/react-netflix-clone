b9331de0cf22bb11e5a77eb6a7f8c47ca5c06695
RidhoAnshory <muhridhoanshory@gmail.com>
2021-11-06 19:17:30 +0800

    add header for browse films & series

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link as ReactRouterLink } from "react-router-dom";
-import { Background, Container, Logo, ButtonL, Feature } from "./styles/header";
+import {
+  Background,
+  Container,
+  Link,
+  Logo,
+  Group,
+  Picture,
+  Profile,
+  Dropdown,
+  ButtonL,
+  PlayButton,
+  Feature,
+  FeatureCallOut,
+  Text,
+  Search,
+  SearchIcon,
+  SearchInput,
+} from "./styles/header";
 
 export default function Header({ bg = true, children, ...restProps }) {
   return bg ? <Background {...restProps}>{children}</Background> : children;
@@ -10,12 +27,27 @@ Header.Frame = function HeaderFrame({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
 };
 
+Header.Group = function HeaderGroup({ children, ...restProps }) {
+  return <Group {...restProps}>{children}</Group>;
+};
+
 Header.Feature = function HeaderFeature({ children, ...restProps }) {
   return <Feature {...restProps}>{children}</Feature>;
 };
 
+Header.FeatureCallOut = function HeaderFeatureCallOut({
+  children,
+  ...restProps
+}) {
+  return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>;
+};
+
 Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }) {
   return <ButtonL {...restProps}>{children}</ButtonL>;
+};
+
+Header.PlayButton = function HeaderPlayButton({ children, ...restProps }) {
+  return <PlayButton {...restProps}>{children}</PlayButton>;
 };
 
 Header.Logo = function HeaderLogo({ to, ...restProps }) {
@@ -23,5 +55,49 @@ Header.Logo = function HeaderLogo({ to, ...restProps }) {
     <ReactRouterLink to={to}>
       <Logo {...restProps} />
     </ReactRouterLink>
+  );
+};
+
+Header.Picture = function HeaderPicture({ src, ...restProps }) {
+  return <Picture {...restProps} src={`/images/users/${src}.png`} />;
+};
+
+Header.Text = function HeaderText({ children, ...restProps }) {
+  return <Text {...restProps}>{children}</Text>;
+};
+
+Header.TextLink = function HeaderTextLink({ children, ...restProps }) {
+  return <Link {...restProps}>{children}</Link>;
+};
+
+Header.Profile = function HeaderProfile({ children, ...restProps }) {
+  return <Profile {...restProps}>{children}</Profile>;
+};
+
+Header.Dropdown = function HeaderDropdown({ children, ...restProps }) {
+  return <Dropdown {...restProps}>{children}</Dropdown>;
+};
+
+Header.Search = function HeaderSearch({
+  searchTerm,
+  setSearchTerm,
+  ...restProps
+}) {
+  const [searchActive, setSearchActive] = useState(false);
+
+  return (
+    <Search {...restProps}>
+      <SearchIcon
+        onClick={() => setSearchActive((searchActive) => !searchActive)}
+      >
+        <img src="/images/icons/search.png" alt="search" />
+      </SearchIcon>
+      <SearchInput
+        value={searchTerm}
+        onChange={({ target }) => setSearchTerm(target.value)}
+        placeholder="Search films and series"
+        active={searchActive}
+      />
+    </Search>
   );
 };
